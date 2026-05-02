cfa98c3c338d983a58e3a476aff569a6f230de5e
Andrs-31 <gtvfiip@gmail.com>
2026-05-01 19:29:27 -0500

    elimina linea erronea

--- frontend/hooks/use-auth.tsx
+++ frontend/hooks/use-auth.tsx
@@ -100,23 +100,9 @@ export function AuthProvider({ children }: { children: ReactNode }) {
       sessionStorage.clear()
       
       setUser(null)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
       router.replace("/")
-=======
-      router.push("/")
->>>>>>> 95d25d3 (Funcion Logout)
-=======
-      router.replace("/")
->>>>>>> 7a8113c (Logout Funcionando 100%)
-=======
-      router.replace("/")
->>>>>>> 7a8113c (Logout Funcionando 100%)
-=======
-      router.replace("/")
->>>>>>> 7a8113c (Logout Funcionando 100%)
+
     }
   }
 
